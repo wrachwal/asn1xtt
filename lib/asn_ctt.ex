2bf14e0e39e156b44ddba711a301c398da2bb271
Waldemar Rachwał <waldemar.rachwal@gmail.com>
2017-03-27 20:34:42 +0200

    Put tref as first arg in all tref_... functions.

--- lib/asn_ctt.ex
+++ lib/asn_ctt.ex
@@ -23,45 +23,45 @@ defmodule ASN.CTT do
 
   def asn_type_use(db) do
     Enum.reduce(db, %{}, fn
-      {type, typedef(name: type) = tdef}, tref -> tref_typedef(tdef, tref)
+      {type, typedef(name: type) = tdef}, tref -> tref_typedef(tref, tdef)
       {_, rec}, tref when elem(rec, 0) in [:valuedef, :classdef, :module] -> tref
       {@venc, _}, tref -> tref
     end)
   end
 
-  defp tref_typedef(typedef(name: name, typespec: spec), tref) do
-    tref_typespec(spec, inckey(tref, name))
+  defp tref_typedef(tref, typedef(name: name, typespec: spec)) do
+    tref |> inckey(name) |> tref_typespec(spec)
   end
 
-  defp tref_typespec(type(def: def), tref) do
-    tref_def(def, tref)
+  defp tref_typespec(tref, type(def: def)) do
+    tref_def(tref, def)
   end
 
-  defp tref_def(:INTEGER, tref), do: tref
-  defp tref_def(:"OCTET STRING", tref), do: tref
-  defp tref_def(:BOOLEAN, tref), do: tref
-  defp tref_def(:NULL, tref), do: tref
-  defp tref_def(:"OBJECT IDENTIFIER", tref), do: tref
-  defp tref_def(:"ObjectDescriptor", tref), do: tref
-  defp tref_def(:ANY, tref), do: tref
-  defp tref_def({:ENUMERATED, _}, tref), do: tref
-  defp tref_def({:"BIT STRING", _}, tref), do: tref
-  defp tref_def(sequence(components: comps, extaddgroup: :undefined), tref) do
+  defp tref_def(tref, :INTEGER), do: tref
+  defp tref_def(tref, :"OCTET STRING"), do: tref
+  defp tref_def(tref, :BOOLEAN), do: tref
+  defp tref_def(tref, :NULL), do: tref
+  defp tref_def(tref, :"OBJECT IDENTIFIER"), do: tref
+  defp tref_def(tref, :"ObjectDescriptor"), do: tref
+  defp tref_def(tref, :ANY), do: tref
+  defp tref_def(tref, {:ENUMERATED, _}), do: tref
+  defp tref_def(tref, {:"BIT STRING", _}), do: tref
+  defp tref_def(tref, sequence(components: comps, extaddgroup: :undefined)) do
     tref_components(tref, comps)
   end
-  defp tref_def(sequence(components: comps, extaddgroup: exts), tref) do
+  defp tref_def(tref, sequence(components: comps, extaddgroup: exts)) do
     tref |> tref_components(comps) |> tref_components(exts)
   end
-  defp tref_def({:"SEQUENCE OF", type() = type}, tref) do
-    tref_typespec(type, tref)
+  defp tref_def(tref, {:"SEQUENCE OF", type() = type}) do
+    tref_typespec(tref, type)
   end
-  defp tref_def({:CHOICE, comps}, tref) when is_list(comps) do
+  defp tref_def(tref, {:CHOICE, comps}) when is_list(comps) do
     tref_components(tref, comps)
   end
-  defp tref_def({:CHOICE, {comps, exts}}, tref) do
+  defp tref_def(tref, {:CHOICE, {comps, exts}}) do
     tref |> tref_components(comps) |> tref_components(exts)
   end
-  defp tref_def(extyperef(type: type), tref) do
+  defp tref_def(tref, extyperef(type: type)) do
     inckey(tref, type)
   end
 
@@ -69,7 +69,7 @@ defmodule ASN.CTT do
     tref |> tref_comp_list(comps) |> tref_comp_list(exts)
   end
   defp tref_components(tref, comps) when is_list(comps) do
-    tref_comp_list(tref, comps)
+    tref |> tref_comp_list(comps)
   end
 
   defp tref_comp_list(tref, [comp | comps]) do
@@ -82,7 +82,7 @@ defmodule ASN.CTT do
   defp tref_comp(tref, extaddgroup()), do: tref
   defp tref_comp(tref, :ExtensionAdditionGroupEnd), do: tref
   defp tref_comp(tref, comptype(typespec: spec)) do
-    tref_typespec(spec, tref)
+    tref_typespec(tref, spec)
   end
 
   defp inckey(map, key) do

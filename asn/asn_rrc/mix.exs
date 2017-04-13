aa67bd87ff0c302ba5e25821fa98206df3bbb584
Waldemar Rachwał <waldemar.rachwal@gmail.com>
2017-04-13 20:54:15 +0200

    Separate .set.asn1 configuration file from .asn files.

--- asn/asn_rrc/mix.exs
+++ asn/asn_rrc/mix.exs
@@ -5,6 +5,7 @@ defmodule ASN.RRC.Mixfile do
     [app: :asn_rrc,
      version: "14.1.0",
      compilers: [:asn1] ++ Mix.compilers,
+     asn1_paths: ["asn1"],
      asn1_options: [:uper,        # Unaligned PER
                     :undec_rest], # to decode MIB/SIB(s) with 0 padding
      deps: deps()]

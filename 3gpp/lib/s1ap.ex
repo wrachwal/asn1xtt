4ee62acace0645c9d83c78482071365727e23a6c
Waldemar Rachwał <waldemar.rachwal@gmail.com>
2018-04-08 11:01:47 +0200

    burn_asn1db for s1ap/x2ap to explore.
    - mechanics of these protocols are quite different from rrc,
      so i am curious how intermediates from asn1_ct look..
    
    ==> asn1xtt
    Compiling 6 files (.ex)
    # X2AP/classdef = 7
    # X2AP/module = 1
    # X2AP/ptypedef = 11
    # X2AP/typedef = 1049
    # X2AP/valuedef = 244
    # S1AP/classdef = 7
    # S1AP/module = 1
    # S1AP/ptypedef = 14
    # S1AP/typedef = 1287
    # S1AP/valuedef = 334
    # RRC/classdef = 2
    # RRC/module = 1
    # RRC/typedef = 1757
    # RRC/valuedef = 142

--- 3gpp/lib/s1ap.ex
+++ 3gpp/lib/s1ap.ex
@@ -1,8 +1,14 @@
+srcdir = Path.join(__DIR__, "../asn/asn_s1ap/src")
+
 defmodule S1AP do
   require ASN.CTT
+  require Record
 
-  @asn1db Path.join(__DIR__, "../asn/asn_s1ap/src/asn_s1ap.asn1db")
-
-  ASN.CTT.burn_asn1db(@asn1db, :db)
-
+  ASN.CTT.burn_asn1db(Path.join(srcdir, "asn_s1ap.asn1db"), :db)
+  |> Keyword.values()
+  |> Enum.filter(&Record.is_record/1)
+  |> Enum.map(&elem(&1, 0))
+  |> Enum.reduce(%{}, fn rec, map -> map |> Map.put_new(rec, 0) |> update_in([rec], &(&1 + 1)) end)
+  |> Enum.sort()
+  |> Enum.each(fn {t, c} -> IO.puts("# S1AP/#{t} = #{c}") end)
 end
--- 3gpp/lib/x2ap.ex
+++ 3gpp/lib/x2ap.ex
@@ -1,8 +1,14 @@
+srcdir = Path.join(__DIR__, "../asn/asn_x2ap/src")
+
 defmodule X2AP do
   require ASN.CTT
+  require Record
 
-  @asn1db Path.join(__DIR__, "../asn/asn_x2ap/src/asn_x2ap.asn1db")
-
-  ASN.CTT.burn_asn1db(@asn1db, :db)
-
+  ASN.CTT.burn_asn1db(Path.join(srcdir, "asn_x2ap.asn1db"), :db)
+  |> Keyword.values()
+  |> Enum.filter(&Record.is_record/1)
+  |> Enum.map(&elem(&1, 0))
+  |> Enum.reduce(%{}, fn rec, map -> map |> Map.put_new(rec, 0) |> update_in([rec], &(&1 + 1)) end)
+  |> Enum.sort()
+  |> Enum.each(fn {t, c} -> IO.puts("# X2AP/#{t} = #{c}") end)
 end
